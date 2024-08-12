577be6e26e884c82a2edb4cedbd91243856aa80a
Shehar-Bano <bshehar2002@gmail.com>
2024-08-12 11:06:48 +0500

    school system

---
+++ resources/views/view-file/head.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Skydash Admin</title>
+    <!-- plugins:')}} -->
+    <link rel="stylesheet" href="{{asset('assesst/vendors/feather/feather.css')}}">
+    <link rel="stylesheet" href="{{asset('assesst/vendors/ti-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('assesst/vendors/css/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- Plugin ')}} for this page -->
+    <link rel="stylesheet" href="{{asset('assesst/vendors/datatables.net-bs4/dataTables.bootstrap4.')}}">
+    <link rel="stylesheet" href="{{asset('assesst/vendors/ti-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" type="text/')}}" href="{{asset('assesst/js/select.dataTables.min.')}}">
+    <!-- End plugin ')}} for this page -->
+    <!-- inject:')}} -->
+    <link rel="stylesheet" href="{{asset('assesst/css/vertical-layout-light/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{asset('assesst/images/favicon.png')}}" />
+  </head>
